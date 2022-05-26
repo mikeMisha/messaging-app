3d5ce66c2ba8e987365eec46d19cdc808d06e9ba
mikeMisha <62260889+mikeMisha@users.noreply.github.com>
2022-05-26 19:54:50 -0400

    fixed bug with messages not showing in order

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-import { SenderBubble, OtherUserBubble } from '.';
-import moment from 'moment';
+import React from "react";
+import { Box } from "@material-ui/core";
+import { SenderBubble, OtherUserBubble } from ".";
+import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+	const { messages, otherUser, userId } = props;
+	//sort messages by date & time
+	const sortedMsgs = [...messages].sort(
+		(a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+	);
+	return (
+		<Box>
+			{sortedMsgs.map((message) => {
+				const time = moment(message.createdAt).format("h:mm");
 
-  return (
-    <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format('h:mm');
-
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
-        );
-      })}
-    </Box>
-  );
+				return message.senderId === userId ? (
+					<SenderBubble key={message.id} text={message.text} time={time} />
+				) : (
+					<OtherUserBubble
+						key={message.id}
+						text={message.text}
+						time={time}
+						otherUser={otherUser}
+					/>
+				);
+			})}
+		</Box>
+	);
 };
 
 export default Messages;
