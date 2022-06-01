9f709f022245185138a10566b7a700908d0d9252
mikeMisha <62260889+mikeMisha@users.noreply.github.com>
2022-06-01 15:02:53 -0400

    fixed format and shallow copy messages

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -83,6 +83,7 @@ const Home = ({ user, logout }) => {
       prev.map((convo) => {
         if (convo.otherUser.id === recipientId) {
           const convoCopy = { ...convo };
+          convoCopy.messages = [...convo.messages];
           convoCopy.messages.push(message);
           convoCopy.latestMessageText = message.text;
           convoCopy.id = message.conversationId;
@@ -110,6 +111,7 @@ const Home = ({ user, logout }) => {
       prev.map((convo) => {
         if (convo.id === message.conversationId) {
           const convoCopy = { ...convo };
+          convoCopy.messages = [...convo.messages];
           convoCopy.messages.push(message);
           convoCopy.latestMessageText = message.text;
           return convoCopy;
