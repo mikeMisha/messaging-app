910b99c2e9499f93ff3580461d15296cabe015de
mikeMisha <62260889+mikeMisha@users.noreply.github.com>
2022-06-15 17:21:51 -0400

    ready to deploy

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,14 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = db;
